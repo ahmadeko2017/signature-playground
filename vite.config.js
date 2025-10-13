2c809a1f706276540bc2b73a3f13e7818a3d9941
Ahmad Eko Kurniawan <ahmadeko2017@mail.ugm.ac.id>
2025-10-13 10:22:44 +0700

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,50 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import mkcert from "vite-plugin-mkcert";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    mkcert(),
+    VitePWA({
+      registerType: "autoUpdate",
+      // aktifkan generate manifest otomatis
+      injectRegister: "auto",
+      manifest: {
+        name: "Signature Playground v6",
+        short_name: "Signature",
+        description: "PKCS#1-compatible Signature Playground – Paylabs",
+        theme_color: "#ffffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        orientation: "portrait",
+        start_url: "/",
+        scope: "/",
+        icons: [
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
+  server: {
+    https: true,
+    host: "10.10.10.61",
+    port: 5173,
+  },
+});
